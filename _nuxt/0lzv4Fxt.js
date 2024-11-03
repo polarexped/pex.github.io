32b578584e159d102d929d19141b2c1bf804525c
Robert <r0b3r70@gmail.com>
2024-11-03 21:13:10 +0100

    nuxt version

---
+++ _nuxt/0lzv4Fxt.js
@@ -0,0 +1,1 @@
+import s from"./3Vw8zM65.js";import{e as o,J as u,l as f,Z as m}from"./DERWrZCB.js";import"./Dnd51l0P.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ _nuxt/CDVOiopP.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DrRrv8Le.js";import"./DERWrZCB.js";export{m as default};
